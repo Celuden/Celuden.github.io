44ed1489a6efac6d2c81a6e6315c2973dbc8c506
Alija Suljic <asuljic.lba@fh-salzburg.ac.at>
2025-03-13 00:41:44 +0100

    implemented the flame n fizzle project page, adjusted alt text in realm of urnest project

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const nextProjectBtn = document.querySelector(".next-project")
 
   // You'll need to replace these with your actual project pages
-  const projectPages = ["MarosMayhem.html", "KnockOffHeroes.html", "ProjectSisyphos.html", "RealmOfUnrest.html"]
+  const projectPages = ["MarosMayhem.html", "KnockOffHeroes.html", "ProjectSisyphos.html", "RealmOfUnrest.html", "FlameNFizzle.html"]
 
   function getCurrentProjectIndex() {
     const currentPage = window.location.pathname.split("/").pop()
